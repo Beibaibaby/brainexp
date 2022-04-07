f4be529143ad720842dd86338df3526efb56f97b
Beibaibaby <56213541+Beibaibaby@users.noreply.github.com>
2022-04-07 17:43:50 -0400

    first_two

--- pca/read_after_stress.py
+++ pca/read_after_stress.py
@@ -55,46 +55,50 @@ with open('/Users/dragon/Desktop/brainexp/behav/w2.csv') as f:
 
 myarray = np.asarray(myarray,dtype=np.float32)
 
-
-idx=1
 pca = PCA(n_components=5)
 ts_transformed = pca.fit_transform(ts)
 np.save('./series/afterstress/pca', pca.components_)
-#print('transform shape')
-#print(ts_transformed.shape)
-ccc=['blue','red','green','brown','purple']
-dt=1
+# print('transform shape')
+# print(ts_transformed.shape)
+ccc = ['blue', 'red', 'green', 'brown', 'purple']
+dt = 1
 starttime = 0
 endtime = 8995
 steps = int(abs(starttime - endtime) / dt)
 time = np.linspace(starttime, endtime, steps)
 print(time.shape)
-r2=get_r2_statsmodels(ts_transformed.T[idx-1],myarray[:time.shape[0]])
-fig, ax = plt.subplots()
-a=[10,193,371,1261,1488,1722,2933,3097,3257,3364,3745,4034,4366,5279,5656,5781,6066,6349,6469,7111,7375,7711,7912,8026,8194,8379,8744]
-b=[51,207,442,1342,1542,1764,2965,3135,3281,3412,3867,4098,4458,5360,5757,5800,6081,6406,6507,7200,7401,7744,7959,8069,8252,8400,8843]
-for i in range(len(a)):
- ax.axvspan(a[i], b[i], alpha=0.3, color='green')
+ccc = ['blue', 'red', 'green', 'brown', 'purple']
 
-ccc=['blue','red','green','brown','purple']
-#plt.plot(time,smoothing(ts_transformed.T[0],50),'blue',label='pc1-'+str(pca.explained_variance_ratio_[0]),markersize=3)
-ax.plot(time,smoothing(ts_transformed.T[idx-1],50),ccc[idx-1],label='ER'+str(float (int (pca.explained_variance_ratio_[idx-1] * 1000) / 1000))+' SD-'+str(dig(np.std(ts_transformed.T[idx-1]))),markersize=3)
-
-#plt.plot(time,smoothing(pca.components_[1],100),'red',label='pc2-'+str(pca.explained_variance_ratio_[1]),markersize=3)
-#plt.plot(time,smoothing(pca.components_[2],100),'green',label='pc3-'+str(pca.explained_variance_ratio_[2]),markersize=3)
-#plt.plot(time,smoothing(pca.components_[3]),'orange',label='pc2-'+str(pca.explained_variance_ratio_[3])',markersize=3)
+for idx in [1,2,3,4,5]:
+    r2 = get_r2_statsmodels(ts_transformed.T[idx - 1], myarray[:time.shape[0]])
+    fig, ax = plt.subplots()
+    a = [10, 193, 371, 1261, 1488, 1722, 2933, 3097, 3257, 3364, 3745, 4034, 4366, 5279, 5656, 5781, 6066, 6349, 6469,
+         7111, 7375, 7711, 7912, 8026, 8194, 8379, 8744]
+    b = [51, 207, 442, 1342, 1542, 1764, 2965, 3135, 3281, 3412, 3867, 4098, 4458, 5360, 5757, 5800, 6081, 6406, 6507,
+         7200, 7401, 7744, 7959, 8069, 8252, 8400, 8843]
+    for i in range(len(a)):
+        ax.axvspan(a[i], b[i], alpha=0.3, color='green')
 
 
-#plt.title('Before stress: PC (smoothing =50)')
-ax.set_xlabel('times',fontsize=14)
-ax.set_ylabel('pc',fontsize=14)
+    # plt.plot(time,smoothing(ts_transformed.T[0],50),'blue',label='pc1-'+str(pca.explained_variance_ratio_[0]),markersize=3)
+    ax.plot(time, smoothing(ts_transformed.T[idx - 1], 50), ccc[idx - 1],
+            label='ER' + str(float(int(pca.explained_variance_ratio_[idx - 1] * 1000) / 1000)) + ' SD-' + str(
+                dig(np.std(ts_transformed.T[idx - 1]))), markersize=3)
 
-ax2=ax.twinx()
-ax2.plot(time, smoothing(myarray[:time.shape[0]],50),color="black",label='whisking',alpha=0.5,markersize=3)
-ax2.set_ylabel("magnitude",fontsize=14)
-fig.legend()
-plt.title('PC'+str(idx)+'-After Stress R2='+str(float (int (r2 * 1000) / 1000)))
-plt.show()
+    # plt.plot(time,smoothing(pca.components_[1],100),'red',label='pc2-'+str(pca.explained_variance_ratio_[1]),markersize=3)
+    # plt.plot(time,smoothing(pca.components_[2],100),'green',label='pc3-'+str(pca.explained_variance_ratio_[2]),markersize=3)
+    # plt.plot(time,smoothing(pca.components_[3]),'orange',label='pc2-'+str(pca.explained_variance_ratio_[3])',markersize=3)
 
-plt.savefig('./series/afterstress/pc'+str(idx)+'.png')
-plt.show()
+    # plt.title('Before stress: PC (smoothing =50)')
+    ax.set_xlabel('times', fontsize=14)
+    ax.set_ylabel('pc', fontsize=14)
+
+    ax2 = ax.twinx()
+    ax2.plot(time, smoothing(myarray[:time.shape[0]], 50), color="black", label='whisking', alpha=0.5, markersize=3)
+    ax2.set_ylabel("magnitude", fontsize=14)
+    fig.legend()
+    plt.title('PC' + str(idx) + '-After Stress R2=' + str(float(int(r2 * 1000) / 1000)))
+
+    plt.savefig('./series/afterstress/pc' + str(idx) + '.png')
+    plt.show()
+
--- pca/read_before_stress.py
+++ pca/read_before_stress.py
@@ -55,47 +55,46 @@ with open('/Users/dragon/Desktop/brainexp/behav/w1.csv') as f:
         myarray.append(line)
 
 myarray = np.asarray(myarray,dtype=np.float32)
-
-
-idx=1
 pca = PCA(n_components=5)
 ts_transformed = pca.fit_transform(ts)
 np.save('./series/beforestress/pca', pca.components_)
-#print('transform shape')
-#print(ts_transformed.shape)
-ccc=['blue','red','green','brown','purple']
-dt=1
+
+ccc = ['blue', 'red', 'green', 'brown', 'purple']
+dt = 1
 starttime = 0
 endtime = 8995
 steps = int(abs(starttime - endtime) / dt)
 time = np.linspace(starttime, endtime, steps)
 print(time.shape)
-r2=get_r2_statsmodels(ts_transformed.T[idx-1],myarray[:time.shape[0]])
-fig, ax = plt.subplots()
-a=[22,266,512,1561,2237,2959,3393,3580,3760,5133,5565]
-b=[117,355,602,1628,2389,3010,3475,3633,3827,5284,5692]
-for i in range(len(a)):
- ax.axvspan(a[i], b[i], alpha=0.3, color='green')
+ccc = ['blue', 'red', 'green', 'brown', 'purple']
+for idx in [1,2,3,4,5]:
 
-ccc=['blue','red','green','brown','purple']
-#plt.plot(time,smoothing(ts_transformed.T[0],50),'blue',label='pc1-'+str(pca.explained_variance_ratio_[0]),markersize=3)
-ax.plot(time,smoothing(ts_transformed.T[idx-1],50),ccc[idx-1],label='ER'+str(float (int (pca.explained_variance_ratio_[idx-1] * 1000) / 1000))+' SD-'+str(dig(np.std(ts_transformed.T[idx-1]))),markersize=3)
-
-#plt.plot(time,smoothing(pca.components_[1],100),'red',label='pc2-'+str(pca.explained_variance_ratio_[1]),markersize=3)
-#plt.plot(time,smoothing(pca.components_[2],100),'green',label='pc3-'+str(pca.explained_variance_ratio_[2]),markersize=3)
-#plt.plot(time,smoothing(pca.components_[3]),'orange',label='pc2-'+str(pca.explained_variance_ratio_[3])',markersize=3)
+    r2 = get_r2_statsmodels(ts_transformed.T[idx - 1], myarray[:time.shape[0]])
+    fig, ax = plt.subplots()
+    a = [22, 266, 512, 1561, 2237, 2959, 3393, 3580, 3760, 5133, 5565]
+    b = [117, 355, 602, 1628, 2389, 3010, 3475, 3633, 3827, 5284, 5692]
+    for i in range(len(a)):
+        ax.axvspan(a[i], b[i], alpha=0.3, color='green')
 
 
-#plt.title('Before stress: PC (smoothing =50)')
-ax.set_xlabel('times',fontsize=14)
-ax.set_ylabel('pc',fontsize=14)
+    # plt.plot(time,smoothing(ts_transformed.T[0],50),'blue',label='pc1-'+str(pca.explained_variance_ratio_[0]),markersize=3)
+    ax.plot(time, smoothing(ts_transformed.T[idx - 1], 50), ccc[idx - 1],
+            label='ER' + str(float(int(pca.explained_variance_ratio_[idx - 1] * 1000) / 1000)) + ' SD-' + str(
+                dig(np.std(ts_transformed.T[idx - 1]))), markersize=3)
 
-ax2=ax.twinx()
-ax2.plot(time, smoothing(myarray[:time.shape[0]],50),color="black",label='whisking',alpha=0.5,markersize=3)
-ax2.set_ylabel("magnitude",fontsize=14)
-fig.legend()
-plt.title('PC'+str(idx)+'-Before Stress R2='+str(float (int (r2 * 1000) / 1000)))
-plt.show()
+    # plt.plot(time,smoothing(pca.components_[1],100),'red',label='pc2-'+str(pca.explained_variance_ratio_[1]),markersize=3)
+    # plt.plot(time,smoothing(pca.components_[2],100),'green',label='pc3-'+str(pca.explained_variance_ratio_[2]),markersize=3)
+    # plt.plot(time,smoothing(pca.components_[3]),'orange',label='pc2-'+str(pca.explained_variance_ratio_[3])',markersize=3)
 
-plt.savefig('./series/beforestress/pc'+str(idx)+'.png')
-plt.show()
+    # plt.title('Before stress: PC (smoothing =50)')
+    ax.set_xlabel('times', fontsize=14)
+    ax.set_ylabel('pc', fontsize=14)
+
+    ax2 = ax.twinx()
+    ax2.plot(time, smoothing(myarray[:time.shape[0]], 50), color="black", label='whisking', alpha=0.5, markersize=3)
+    ax2.set_ylabel("magnitude", fontsize=14)
+    fig.legend()
+    plt.title('PC' + str(idx) + '-Before Stress R2=' + str(float(int(r2 * 1000) / 1000)))
+
+    plt.savefig('./series/beforestress/pc' + str(idx) + '.png')
+    plt.show()
